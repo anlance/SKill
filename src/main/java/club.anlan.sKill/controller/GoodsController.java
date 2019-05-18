b8d366295126ac04b4b15ee64f5896033328e1d6
anlan <anlan1996@qq.com>
2019-05-18 10:27:04 +0800

    页面缓存

--- src/main/java/club.anlan.sKill/controller/GoodsController.java
+++ src/main/java/club.anlan.sKill/controller/GoodsController.java
@@ -1,16 +1,24 @@
 package club.anlan.sKill.controller;
 
 import club.anlan.sKill.domain.User;
+import club.anlan.sKill.redis.GoodsKey;
 import club.anlan.sKill.service.GoodsService;
 import club.anlan.sKill.service.RedisService;
 import club.anlan.sKill.service.UserService;
 import club.anlan.sKill.vo.GoodsVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
@@ -27,22 +35,44 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
-    @RequestMapping("/to_list")
-    public String toList(Model model, User user) {
-        model.addAttribute("user",user);
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String toList(HttpServletRequest request, HttpServletResponse response, Model model, User user) {
+        model.addAttribute("user", user);
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
         //查询商品列表
         List<GoodsVo> goodsVoList = goodsService.getGoodsVo();
         model.addAttribute("goodsList", goodsVoList);
-        return "goods_list";
+        //手动渲染
+        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
 
-    @RequestMapping("/to_detail/{goodsId}")
-    public String toDetail(Model model, User user, @PathVariable("goodsId") long goodsId) {
-        model.addAttribute("user",user);
+    @RequestMapping(value = "/to_detail/{goodsId}", produces = "text/html")
+    @ResponseBody
+    public String toDetail(HttpServletRequest request, HttpServletResponse response, Model model, User user, @PathVariable("goodsId") long goodsId) {
+        model.addAttribute("user", user);
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsDetail, "" + goodsId, String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+
         //查询商品列表
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
-        model.addAttribute("goods",goods);
+        model.addAttribute("goods", goods);
 
         long startTime = goods.getStartDate().getTime();
         long endTime = goods.getEndDate().getTime();
@@ -52,17 +82,22 @@ public class GoodsController {
         if (nowTime < startTime) {//秒杀还未开始
             skillStatus = 0;
             remainSeconds = (int) (nowTime - startTime) / 1000;
-        } else if(nowTime>endTime) {//秒杀已经结束
-            skillStatus=2;
-            remainSeconds=-1;
+        } else if (nowTime > endTime) {//秒杀已经结束
+            skillStatus = 2;
+            remainSeconds = -1;
         } else {//秒杀进行中
-            skillStatus=1;
-            remainSeconds=0;
+            skillStatus = 1;
+            remainSeconds = 0;
         }
-        model.addAttribute("skillStatus",skillStatus);
-        model.addAttribute("remainSeconds",remainSeconds);
-        return "goods_detail";
+        model.addAttribute("skillStatus", skillStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        //手动渲染
+        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsDetail, "", html);
+        }
+        return html;
     }
-
 
 }
